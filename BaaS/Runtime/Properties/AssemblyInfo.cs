184f8d12bbad8a150cc68577c8564ac9f7b9ca4f
nikhilk <nikhilk@live.com>
2013-05-28 02:33:47 -0700

    Initial commit

---
+++ BaaS/Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿// AssemblyInfo.cs
+//
+
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("SimpleCloud")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCloud")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ScriptAssembly("Runtime")]
+[assembly: ScriptTemplate(@"
+// {name}.js {version}
+
+{dependenciesLookup}
+var $global = this;
+
+{script}
+")]
