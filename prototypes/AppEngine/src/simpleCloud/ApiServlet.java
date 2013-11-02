70e9be48aeb4e652555aa923fedade13ede42206
Nikhil Kothari <nikhilk@live.com>
2013-11-02 09:35:43 -0700

    Add some script execution

--- prototypes/AppEngine/src/simpleCloud/ApiServlet.java
+++ prototypes/AppEngine/src/simpleCloud/ApiServlet.java
@@ -2,12 +2,47 @@ package simpleCloud;
 
 import java.io.*;
 import javax.servlet.http.*;
+import org.mozilla.javascript.*;
 
 @SuppressWarnings("serial")
-public class ApiServlet extends HttpServlet {
+public final class ApiServlet extends HttpServlet {
+    
+    private static String appScript =
+        "function app() {\n" +
+        "  return (new Date()).toString();\n" +
+        "}\n" +
+        "app()";
 
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, API Caller!");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String text = executeScript();
+        
+        response.setContentType("text/plain");
+        response.getWriter().println(text);
+    }
+    
+    private String executeScript() {
+        Context scriptContext = Context.enter();
+        
+        try {
+            scriptContext.setClassShutter(new SuppressAccessClassShutter());
+            
+            Scriptable scriptScope = scriptContext.initStandardObjects();
+            Object result = scriptContext.evaluateString(scriptScope, appScript, "<app>", 1, null);
+            
+            return Context.toString(result);
+        }
+        catch (RhinoException e) {
+            return e.getMessage();
+        }
+        finally {
+            Context.exit();
+        }
+    }
+    
+    private final class SuppressAccessClassShutter implements ClassShutter {
+        
+        public boolean visibleToScripts(String name) {
+            return false;
+        }
     }
 }
